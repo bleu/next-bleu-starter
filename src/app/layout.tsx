6ae2b4142bf3b7f8953f2a65dbab90ca6550e3f8
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2025-02-05 13:01:12 -0300

    chore: add clsx and custom font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const rubik = Rubik({
+  weight: ["400", "500", "700"],
   subsets: ["latin"],
 });
 
@@ -25,7 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={cn(
+          rubik.className,
+          "bg-background text-foreground px-4 md:px-8"
+        )}
+      >
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
