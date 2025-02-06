0b3d2cd022216289f7b2773475d7e47318d21f6f
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2025-02-06 10:24:12 -0300

    chore: center header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,13 +12,13 @@ const Header = () => {
   const { toggleTheme } = useThemeStore();
 
   return (
-    <header className="flex h-16 items-center justify-between bg-content mx-5 mt-6 px-5 rounded-3xl">
+    <header className="flex relative h-16 items-center justify-between bg-content mx-5 mt-6 px-5 rounded-3xl ">
       <Link href="/">
         <h1 className="text-primary font-bold text-lg font-roboto-mono text-center">
           Bleu
         </h1>
       </Link>
-      <div className="flex gap-4 text-lg">
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-4 text-lg">
         <NavLink href="/">Home</NavLink>
         <NavLink href="/theme-showcase">Theme Showcase</NavLink>
       </div>
